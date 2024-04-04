d731124eb165af75d968d233510a5aa19e169e40
eabauti2 <eabauti2@asu.edu>
2024-04-04 03:14:30 -0700

    Added Add Patient View

---
+++ src/application/NewPatientView.java
@@ -0,0 +1,110 @@
+package application;
+
+import application.ViewController.Views;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+
+public class NewPatientView  extends View{
+
+	VBox vLayout;
+	VBox firstNameLayout;
+	VBox lastNameLayout;
+	VBox dOBLayout;
+	HBox middleLayout;
+	HBox hBtns;
+	
+	Text welcomeTxt;
+	Text enterFirst;
+	Text enterLast;
+	Text enterBirth;
+	
+	TextField firstName;
+	TextField lastName;
+	TextField dateOfBirth;
+	
+	Button backButton;
+	Button addButton;
+	
+	@Override
+	public Parent generate() {
+		
+		vLayout = new VBox(35);
+		firstNameLayout = new VBox(10);
+		lastNameLayout = new VBox(10);
+		dOBLayout = new VBox(10);
+		middleLayout = new HBox(10);
+		hBtns = new HBox(20);
+		
+		welcomeTxt = new Text("Add New Patient");
+		welcomeTxt.setFont(Font.font("Arial", 26));
+		
+		enterFirst = new Text("First Name");
+		enterFirst.setTextAlignment(TextAlignment.CENTER);
+		
+		enterLast = new Text("Last Name");
+		enterLast.setTextAlignment(TextAlignment.CENTER);
+		
+		enterBirth = new Text("Day of Birth");
+		enterBirth.setTextAlignment(TextAlignment.CENTER);
+		
+		firstName = new TextField();
+		firstName.setMinSize(200, 50);
+		firstName.setMaxWidth(100);
+		
+		lastName = new TextField();
+		lastName.setMinSize(200, 50);
+		lastName.setMaxWidth(100);
+		
+		dateOfBirth = new TextField();
+		dateOfBirth.setMinSize(200, 50);
+		dateOfBirth.setMaxWidth(100);
+
+		backButton = new Button("Back");
+		addButton = new Button("Add");
+		
+		backButton.setPrefSize(150, 50);
+		addButton.setPrefSize(150, 50);
+		
+		Font btnFont = Font.font("Arial", 14);
+		backButton.setFont(btnFont);
+		addButton.setFont(btnFont);
+		
+		backButton.setOnAction(e -> ViewController.switchView(Views.INITIAL));
+		addButton.setOnAction(e -> ViewController.switchView(Views.INITIAL));
+		
+		firstNameLayout.getChildren().addAll(enterFirst, firstName);
+		lastNameLayout.getChildren().addAll(enterLast, lastName);
+		dOBLayout.getChildren().addAll(enterBirth, dateOfBirth);
+		
+		VBox temp = new VBox(20);
+		temp.getChildren().addAll(firstNameLayout, lastNameLayout, dOBLayout);
+		temp.setAlignment(Pos.CENTER);
+		
+		middleLayout.getChildren().add(temp);
+		middleLayout.setAlignment(Pos.CENTER);
+		
+		hBtns.getChildren().addAll(backButton, addButton);
+		hBtns.setAlignment(Pos.CENTER);
+		
+		vLayout.getChildren().addAll(welcomeTxt, middleLayout, hBtns);
+		vLayout.setAlignment(Pos.CENTER);
+		
+		vLayout.setStyle("-fx-background-color: linear-gradient(from 41px 34px to 50px 50px, reflect,  #a1ffd3 30%, #ffe5c4 47%);");
+		root = vLayout;
+		return root;
+	}
+
+	@Override
+	public void reset() {
+		
+	}
+
+}
--- src/application/ViewController.java
+++ src/application/ViewController.java
@@ -5,6 +5,11 @@ public class ViewController {
 	private static View LOGIN_VIEW = new LoginView();
 	private static View STAFF_PORTAL_VIEW = new StaffPortalView();
 	private static View PATIENT_LOOKUP_VIEW = new PatientLookupView();
+<<<<<<< Updated upstream
+=======
+	private static View INBOX_VIEW = new InboxView();
+	private static View ADD_PATIENT_VIEW = new NewPatientView();
+>>>>>>> Stashed changes
 	
 	private static View CURRENT_VIEW = null;
 	
@@ -14,7 +19,13 @@ public class ViewController {
 		STAFF_LOGIN,
 		PATIENT_PORTAL,
 		STAFF_PORTAL,
+<<<<<<< Updated upstream
 	    PATIENT_LOOKUP
+=======
+	    PATIENT_LOOKUP,
+	    INBOX,
+	    ADD_PATIENT
+>>>>>>> Stashed changes
 	}
 	
 	public static void switchView(Views newView) {
@@ -37,6 +48,14 @@ public class ViewController {
 		   case PATIENT_LOOKUP:
 			    view = PATIENT_LOOKUP_VIEW;
 			    break;
+<<<<<<< Updated upstream
+=======
+		   case INBOX:
+			   view = INBOX_VIEW;
+			   break;
+		   case ADD_PATIENT:
+			   view = ADD_PATIENT_VIEW;
+>>>>>>> Stashed changes
 			default:
 				System.out.println("ViewController: No view option for " + newView.toString());
 				break;
